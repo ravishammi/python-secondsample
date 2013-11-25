1441bbb4ec07e1ddcb5f854beb01dbc53052a13d
unknown <ravis@INLTRAVISHANKAR.in.nds.com>
2013-11-25 17:24:13 +0530

    Renamed file

---
+++ application.py
@@ -0,0 +1,11 @@
+
+
+
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+
+    start_response(status, headers)
+
+	yield "Hello World.\n"
+
